173e0cfe8da73a9a9a6ed1a17f999b12784eef64
Thao Nguyen <nthao4744@gmail.com>
2026-05-27 23:06:19 +0700

    fix

--- Backend/dist/config/firebaseAdmin.js
+++ Backend/dist/config/firebaseAdmin.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var admin = require("firebase-admin");
-var serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+};
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
