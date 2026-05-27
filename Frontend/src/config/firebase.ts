417bd2d1c46a0ecb25e102280b7577b40ae12c1c
Thao Nguyen <nthao4744@gmail.com>
2026-05-27 23:36:19 +0700

    fix frontend render deploy

--- Frontend/src/config/firebase.ts
+++ Frontend/src/config/firebase.ts
@@ -16,6 +16,8 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log('ENV', import.meta.env);
+console.log('FIREBASE', import.meta.env.VITE_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
